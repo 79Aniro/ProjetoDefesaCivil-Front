37859b45bb31753effccb236cef9e450b11d34b2
79Aniro <capablanca1@bol.com.br>
2018-05-14 23:32:14 -0300

    Criando pagina ocorrencias abertas

--- ProjetoDefesaCivil-App/src/app/app.module.ts
+++ ProjetoDefesaCivil-App/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http'
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
@@ -20,6 +21,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
