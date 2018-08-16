bf31aa0eb4d383188ff97f826936e55d899ef14f
79Aniro <capablanca1@bol.com.br>
2018-08-16 14:24:34 -0300

    Preparando para criar apk

--- ProjetoDefesaCivil-App/src/config/api.config.ts
+++ ProjetoDefesaCivil-App/src/config/api.config.ts
@@ -2,7 +2,7 @@ export const API_CONFIG={
 
     baseUrl:"http://localhost:8080",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/projeto-defesacivil-sjc",
-    herokuBaseUrl:"http://localhost:8080"
+    herokuBaseUrl:"https://defesa-civil-acm.herokuapp.com"
    
 //"https://defesa-civil-acm.herokuapp.com"
 
